ae40d76ee2364dfaec82213299309321bc12779c
Vwing <vwing@uci.edu>
2015-01-24 01:08:10 -0800

    shot

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Movement : MonoBehaviour {
     Vector3 curPos;
     Vector3 mousePos;
-    public float speed;
+    Vector3 rotation;
+    public float speed = 6f;
+    public float rotateSpeed = 5f;
     float zPos;
 	// Use this for initialization
 	void Start () {
@@ -13,19 +15,16 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.position = Vector3.Lerp(transform.position, mousePos, speed * Time.deltaTime);
-        //transform.position += (mousePos - curPos).normalized * speed * Time.deltaTime;
+        //float h = Input.GetAxis("Mouse X") * rotateSpeed;
+        //float z = Input.GetAxis("Mouse X") * rotateSpeed;
+        //transform.Rotate(0, 0, -z);
 	}
     void FixedUpdate()
     {
         curPos = transform.position;
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = zPos;
-        rigidbody2D.AddForce((mousePos - curPos).normalized * speed);
+        rotation = (mousePos - curPos).normalized;
+        rigidbody2D.velocity = rotation * speed;
     }
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.name == "Right")
-    //        transform.position += Vector3.left;
-    //}
 }
---
+++ Assets/ShootGlop.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShootGlop : MonoBehaviour {
+
+    public GameObject glop;
+    public float glopSpeed = 5f;
+    List<GameObject> glopList;
+    float yMax = 7.5f;
+
+	// Use this for initialization
+    void Start()
+    {
+        glopList = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+            FireGlop();
+	}
+
+    void FireGlop()
+    {
+        if (glopList.Count <= 5)
+        {
+            GameObject temp = Instantiate(glop, transform.position, transform.rotation) as GameObject;
+            temp.rigidbody2D.velocity = Vector3.up * glopSpeed;
+            glopList.Add(temp);
+        }
+    }
+}
