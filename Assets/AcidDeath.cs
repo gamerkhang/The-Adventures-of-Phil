42cb2699703e0b2d9ac076584d038e085977c033
Vwing <vwing@uci.edu>
2015-01-24 10:04:47 -0800

    Added Acid Death

---
+++ Assets/AcidDeath.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AcidDeath : MonoBehaviour
+{
+    public GameObject splash;
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Acid"))
+        {
+            GameObject temp = Instantiate(splash,splash.transform.position,splash.transform.rotation) as GameObject; //, new Vector3(transform.position.x, transform.position.y, 0, transform.rotation
+            temp.SetActive(true);
+            Destroy(temp,1f);
+            Destroy(gameObject);
+        }
+    }
+}
