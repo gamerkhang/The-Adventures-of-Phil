78cbece5bcbf041940e8d6b990ca007da6665992
Khang Tran <gamerkhang@gmail.com>
2015-01-24 01:02:27 -0800

    Pause Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+	public bool gameRunning = true;
+	GameObject pauseMenu;
 
 	// Use this for initialization
 	void Start () {
-	
+		pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
+		pauseMenu.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown("Pause"))
+		{
+			Time.timeScale = 0;
+			gameRunning = false;
+			pauseMenu.SetActive(true);
+		}
+	}
+
+	public void Unpause () {
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1;
+		gameRunning = true;
 	}
 }
